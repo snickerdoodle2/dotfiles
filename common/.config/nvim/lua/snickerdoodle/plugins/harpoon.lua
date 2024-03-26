27caa483196e7d145fdaad54e50942ac40eac784
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-03-26 14:57:45 +0100

    feat: harpoon tmp oil.nvim fix

--- common/.config/nvim/lua/snickerdoodle/plugins/harpoon.lua
+++ common/.config/nvim/lua/snickerdoodle/plugins/harpoon.lua
@@ -8,7 +8,51 @@ return {
     config = function()
         local harpoon = require("harpoon")
 
-        harpoon:setup({})
+        harpoon:setup({
+            default = {
+                select = function(list_item, list, options)
+                    local _ = list
+                    options = options or {}
+                    if list_item == nil then
+                        return
+                    end
+
+                    local bufnr = vim.fn.bufnr(list_item.value)
+                    local set_position = false
+                    if bufnr == -1 then
+                        set_position = true
+                        bufnr = vim.fn.bufnr(list_item.value, true)
+                    end
+                    if not vim.api.nvim_buf_is_loaded(bufnr) then
+                        vim.fn.bufload(bufnr)
+                        vim.api.nvim_set_option_value("buflisted", true, {
+                            buf = bufnr,
+                        })
+                    end
+
+                    if options.vsplit then
+                        vim.cmd("vsplit")
+                    elseif options.split then
+                        vim.cmd("split")
+                    elseif options.tabedit then
+                        vim.cmd("tabedit")
+                    end
+
+                    vim.api.nvim_set_current_buf(bufnr)
+
+                    if list_item.value:find("^oil://") ~= nil then
+                        vim.api.nvim_win_set_cursor(0, { 1, 10 })
+                    end
+
+                    if set_position then
+                        vim.api.nvim_win_set_cursor(0, {
+                            list_item.context.row or 1,
+                            list_item.context.col or 0,
+                        })
+                    end
+                end,
+            }
+        })
 
         vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
