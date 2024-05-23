90f249e103dc3efea4886054f0cec4ca0b0f7f22
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-05-23 10:20:42 +0200

    feat: disabled surround (used it like 5 times maxx)

--- common/.config/nvim/lua/snickerdoodle/plugins/editor/minisurround.lua
+++ common/.config/nvim/lua/snickerdoodle/plugins/editor/minisurround.lua
@@ -2,6 +2,7 @@ return {
     'echasnovski/mini.surround',
     event = { "BufReadPre", "BufNewFile" },
     version = '*',
+    enabled = false,
     config = function()
         require('mini.surround').setup()
     end
