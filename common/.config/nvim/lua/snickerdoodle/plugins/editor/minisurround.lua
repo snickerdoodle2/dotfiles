03fa7b879528005fcfdc942ef976d5e62ce6d6f7
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-05-23 12:33:06 +0200

    feat: mini change to use leader

--- common/.config/nvim/lua/snickerdoodle/plugins/editor/minisurround.lua
+++ common/.config/nvim/lua/snickerdoodle/plugins/editor/minisurround.lua
@@ -2,8 +2,14 @@ return {
     'echasnovski/mini.surround',
     event = { "BufReadPre", "BufNewFile" },
     version = '*',
-    enabled = false,
-    config = function()
-        require('mini.surround').setup()
-    end
+    opts = {
+        mappings = {
+            add = '<leader>sa',       -- Add surrounding in Normal and Visual modes
+            delete = '<leader>sd',    -- Delete surrounding
+            find = '<leader>sf',      -- Find surrounding (to the right)
+            find_left = '<leader>sF', -- Find surrounding (to the left)
+            highlight = '<leader>sh', -- Highlight surrounding
+            replace = '<leader>sr',   -- Replace surrounding
+        }
+    }
 }
