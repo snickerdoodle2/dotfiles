36fb839b76cf85951265de0a79242f5ab1bc5f3e
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-05-23 13:03:39 +0200

    feat: dropbar.nvim

---
+++ common/.config/nvim/lua/snickerdoodle/plugins/ui/dropbar.lua
@@ -0,0 +1,31 @@
+return {
+    'Bekaboo/dropbar.nvim',
+    -- optional, but required for fuzzy finder support
+    dependencies = {
+        'nvim-telescope/telescope-fzf-native.nvim'
+    },
+    opts = {
+        bar = {
+            sources = function(buf, _)
+                local sources = require('dropbar.sources')
+                local utils = require('dropbar.utils')
+                if vim.bo[buf].ft == 'markdown' then
+                    return {
+                        sources.markdown,
+                    }
+                end
+                if vim.bo[buf].buftype == 'terminal' then
+                    return {
+                        sources.terminal,
+                    }
+                end
+                return {
+                    utils.source.fallback({
+                        sources.lsp,
+                        sources.treesitter,
+                    }),
+                }
+            end
+        }
+    }
+}
