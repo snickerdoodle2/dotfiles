79461f32ea471aef0ec98986ac73078d790bca5e
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-09-16 20:59:29 +0200

    feat: basic nix configuration

---
+++ mac/.config/nix-darwin/flake.nix
@@ -0,0 +1,51 @@
+{
+    description = "Example Darwin system flake";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        nix-darwin.url = "github:LnL7/nix-darwin";
+        nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    outputs = inputs@{ self, nix-darwin, nixpkgs }:
+        let
+        configuration = { pkgs, ... }: {
+            environment.systemPackages = with pkgs; [ 
+                neovim
+                stow
+                zoxide
+                fnm
+                lsd
+                ripgrep
+                tmux
+                lazygit
+                ];
+
+            services.nix-daemon.enable = true;
+            nix.settings.experimental-features = "nix-command flakes";
+            programs.zsh.enable = true;  
+            system.configurationRevision = self.rev or self.dirtyRev or null;
+            system.stateVersion = 5;
+            nixpkgs.hostPlatform = "aarch64-darwin";
+            security.pam.enableSudoTouchIdAuth = true;
+
+            homebrew = {
+                enable = true;
+                onActivation.cleanup = "uninstall";
+                taps = [];
+                brews = [];
+                casks = [ "alacritty" ];
+            };
+        };
+    in
+    {
+# Build darwin flake using:
+# $ darwin-rebuild build --flake .#dominik-mb
+        darwinConfigurations."dominik-mb" = nix-darwin.lib.darwinSystem {
+            modules = [ configuration ];
+        };
+
+# Expose the package set, including overlays, for convenience.
+        darwinPackages = self.darwinConfigurations."dominik-mb".pkgs;
+    };
+}
