f8439347c487648651135a4d3ae5d78be2756573
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-09-26 13:40:17 +0200

    feat: polkit enabled

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -90,6 +90,23 @@
   (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
 
+  security.polkit.enable = true;
+  systemd = {
+      user.services.polkit-gnome-authentication-agent = {
+          description = "polkit-gnome-authentication-agent";
+          wantedBy = [ "graphical-session.target" ];
+          wants = [ "graphical-session.target" ];
+          after = [ "graphical-session.target" ];
+          serviceConfig = {
+              Type = "simple";
+              ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+              Restart = "on-failure";
+              RestartSec = 1;
+              TimeoutStopSec = 10;
+          };
+      };
+  };
+
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
