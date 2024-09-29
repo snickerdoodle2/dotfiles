86bbfded0c0066fe4aceb0f05eb119257a99f2e0
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-09-29 14:26:22 +0200

    feat: secure boot

--- nix/nixos/flake.nix
+++ nix/nixos/flake.nix
@@ -4,9 +4,15 @@
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
         nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
         zen-browser.url = "github:MarceColl/zen-browser-flake";
+        lanzaboote = {
+            url = "github:nix-community/lanzaboote/v0.4.1";
+
+# Optional but recommended to limit the size of your system closure.
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
 	};
 
-	outputs = { self, nixpkgs, nixpkgs-unstable, ...}@inputs: {
+	outputs = { self, nixpkgs, nixpkgs-unstable, lanzaboote, ...}@inputs: {
 		nixosConfigurations.dominik-pc = nixpkgs.lib.nixosSystem  rec {
 			system = "x86_64-linux";
 
@@ -20,6 +26,20 @@
 			modules = [
 				../packages.nix
 				./configuration.nix
+                lanzaboote.nixosModules.lanzaboote
+                ({ pkgs, lib, ... }: {
+
+                 environment.systemPackages = [
+                 pkgs.sbctl
+                 ];
+
+                 boot.loader.systemd-boot.enable = lib.mkForce false;
+
+                 boot.lanzaboote = {
+                 enable = true;
+                 pkiBundle = "/etc/secureboot";
+                 };
+          })
 			];
 		};
 	};
