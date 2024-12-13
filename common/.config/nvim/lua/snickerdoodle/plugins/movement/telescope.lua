e6aedd625670a97a4dcbe363a3e11e10f35810e4
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-12-13 23:04:29 +0100

    feat: customized telescope layouts

--- common/.config/nvim/lua/snickerdoodle/plugins/movement/telescope.lua
+++ common/.config/nvim/lua/snickerdoodle/plugins/movement/telescope.lua
@@ -13,6 +13,26 @@ return {
     },
     config = function()
         local telescope = require('telescope')
+        telescope.setup({
+            defaults = {
+                layout_config = {
+                    horizontal = {
+                        width = 0.9,
+                    }
+                }
+            },
+            extensions = {
+                undo = {
+                    side_by_side = true,
+                    layout_strategy = "vertical",
+                    layout_config = {
+                        preview_height = 0.6,
+                        preview_cutoff = 0,
+                        height = 0.99,
+                    },
+                }
+            }
+        })
         telescope.load_extension('fzf')
         telescope.load_extension('undo')
     end
