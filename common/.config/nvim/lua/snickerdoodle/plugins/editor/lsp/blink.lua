9ebcca89dc73e4cfe62c9fefa0154cbabc4a7ed0
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-12-14 00:52:12 +0100

    feat: snippets

--- common/.config/nvim/lua/snickerdoodle/plugins/editor/lsp/blink.lua
+++ common/.config/nvim/lua/snickerdoodle/plugins/editor/lsp/blink.lua
@@ -1,6 +1,29 @@
 return {
     'saghen/blink.cmp',
+    dependencies = {
+        "L3MON4D3/LuaSnip"
+    },
     version = 'v0.*',
     lazy = true,
-    opts = {},
+    opts = {
+        snippets = {
+            expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
+            active = function(filter)
+                if filter and filter.direction then
+                    return require('luasnip').jumpable(filter.direction)
+                end
+                return require('luasnip').in_snippet()
+            end,
+            jump = function(direction) require('luasnip').jump(direction) end,
+        },
+        sources = {
+            completion = {
+                enabled_providers = { "lsp", "path", "luasnip", "buffer", "lazydev" },
+            },
+            providers = {
+                lsp = { fallback_for = { "lazydev" } },
+                lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+            },
+        },
+    }
 }
