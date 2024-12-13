c297f33059f5b29f97219b6c41cba660c30c9e52
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-12-13 22:48:46 +0100

    feat: undo tree inside telescope

--- common/.config/nvim/lua/snickerdoodle/plugins/movement/telescope.lua
+++ common/.config/nvim/lua/snickerdoodle/plugins/movement/telescope.lua
@@ -3,13 +3,17 @@ return {
     tag = '0.1.5',
     dependencies = {
         'nvim-lua/plenary.nvim',
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        "debugloop/telescope-undo.nvim",
     },
     keys = {
         { '<leader>ff', function() require('telescope.builtin').find_files() end, {}, mode = 'n', desc='Search files'},
         { '<leader>fs', function() require('telescope.builtin').live_grep() end, {}, mode = 'n', desc='Search a phrase'},
+        { '<leader>u', function() require('telescope').extensions.undo.undo() end, {}, mode = 'n', desc='Undo tree'},
     },
     config = function()
-        require('telescope').load_extension('fzf')
+        local telescope = require('telescope')
+        telescope.load_extension('fzf')
+        telescope.load_extension('undo')
     end
 }
