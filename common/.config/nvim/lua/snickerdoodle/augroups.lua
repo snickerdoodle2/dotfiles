38d48de58296ceab13820bb3a4669e3ed21b60fc
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-12-13 21:15:06 +0100

    revert: removed copy on yank

--- common/.config/nvim/lua/snickerdoodle/augroups.lua
+++ common/.config/nvim/lua/snickerdoodle/augroups.lua
@@ -1,3 +1,12 @@
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = vim.api.nvim_create_augroup('highlight_yank', {}),
+    desc = 'Hightlight selection on yank',
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank { higroup = 'IncSearch', timeout = 100 }
+    end,
+})
+
 vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "BufEnter" }, {
     group = vim.api.nvim_create_augroup("ScrollOffEOF", {}),
     callback = function()
