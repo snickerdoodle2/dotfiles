e8623005966b9b3a498bb9f35d282824afc2f421
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-12-14 21:41:07 +0100

    feat: snacks start

---
+++ common/.config/nvim/lua/snickerdoodle/plugins/snacks.lua
@@ -0,0 +1,41 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    keys = {
+        { '<leader>gB', function() Snacks.gitbrowse() end, desc = 'Browse git' },
+    },
+    opts = {
+        bigfile = {
+            enable = true,
+        },
+        dashboard = {
+            preset = {
+                keys = {
+                    { action = ":Telescope find_files", desc = "Find file", icon = "󱙓 ", key = "f" },
+                    { action = ":lua Snacks.scratch()", desc = "Scratch file", icon = "󰎚 ", key = "n" },
+                    { action = ":Telescope live_grep", desc = "Find text", icon = " ", key = "s" },
+                    { action = ":TodoTelescope", desc = "Find TODOS", icon = "󰄸 ", key = "t" },
+                    { action = ":lua _lazygit_toggle()", desc = "Open git", icon = " ", key = "g" },
+                    { action = ":Lazy", desc = "Lazy", icon = "󰒲 ", key = "l" },
+                    { action = ":qa", desc = "Quit", icon = " ", key = "q" },
+                },
+            },
+            sections = {
+                {
+                    { section = "keys",   gap = 1, padding = 1 },
+                    { section = "startup" }
+                },
+                {
+                    section = "terminal",
+                    cmd = vim.fn.stdpath('config') ..
+                        "/static/pokemon-colorscripts/pokemon-colorscripts.py -r --no-title --shiny",
+                    random = 10,
+                    pane = 2,
+                    indent = 10,
+                    height = 28,
+                },
+            },
+        },
+    },
+}
--- common/.config/nvim/lua/snickerdoodle/plugins/ui/toggleterm.lua
+++ common/.config/nvim/lua/snickerdoodle/plugins/ui/toggleterm.lua
@@ -7,6 +7,11 @@ return {
 
         local terminal = require('toggleterm.terminal').Terminal
         local lazygit = terminal:new({ cmd = "lazygit", hidden = true, direction = 'float' })
+
+        function _lazygit_toggle()
+            lazygit:toggle()
+        end
+
         vim.keymap.set('n', '<leader>gg', function()
             lazygit:toggle()
         end, { desc = "Open Lazygit" })
