ac873d944c9dfc5a79854edbcf938981f822e5d6
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-12-14 02:25:55 +0100

    feat: changed diagnostics icons

--- common/.config/nvim/lua/snickerdoodle/plugins/editor/lsp/lspconfig.lua
+++ common/.config/nvim/lua/snickerdoodle/plugins/editor/lsp/lspconfig.lua
@@ -31,6 +31,12 @@ return {
             end
         })
 
+        local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+        for type, icon in pairs(signs) do
+            local hl = "DiagnosticSign" .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+        end
+
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities())
 
