f0a557ff3c59bb14d010c0406a28e6f0c9db956f
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-12-16 15:46:04 +0100

    feat: treesitter comments, text object and incremental selection

--- common/.config/nvim/lua/snickerdoodle/plugins/editor/treesitter/treesitter.lua
+++ common/.config/nvim/lua/snickerdoodle/plugins/editor/treesitter/treesitter.lua
@@ -1,5 +1,8 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+    },
     build = ":TSUpdate",
     event = "LazyFile",
     init = function(plugin)
@@ -37,7 +40,24 @@ return {
             "xml",
             "yaml",
         },
-        -- TODO: incremental selection
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<C-n>",
+                node_incremental = "<C-n>",
+                scope_incremental = false,
+                node_decremental = "<bs>",
+            },
+        },
+        textobjects = {
+            move = {
+                enable = true,
+                goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+                goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+                goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+                goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+            },
+        },
     },
     config = function(_, opts)
         require('nvim-treesitter.configs').setup(opts)
