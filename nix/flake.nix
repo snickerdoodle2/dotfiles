ce8c7b3cb7d89cad631e44e909d2e530580893d5
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-02-10 13:36:34 +0100

    fix: change darwin to follow unstable

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,7 +4,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs-unstable";
   };
   outputs = inputs @ {
     self,
