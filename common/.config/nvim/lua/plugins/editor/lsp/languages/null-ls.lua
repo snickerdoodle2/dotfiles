e83ef121b7858ab45eaa993d970ef376fc3a2168
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-02-11 19:42:53 +0100

    feat: added conditional credo support

--- common/.config/nvim/lua/plugins/editor/lsp/languages/null-ls.lua
+++ common/.config/nvim/lua/plugins/editor/lsp/languages/null-ls.lua
@@ -12,13 +12,25 @@ return {
         "vue",
         "svelte",
         "astro",
+        "elixir",
     },
     opts = function()
+        local null_ls = require('null-ls')
+        local sources = {
+            require('none-ls.diagnostics.eslint_d'),
+            require('none-ls.code_actions.eslint_d'),
+        }
+        local add_cond = function(source, cond)
+            if cond then
+                table.insert(sources, source)
+            end
+        end
+
+        add_cond(null_ls.builtins.diagnostics.credo,
+            vim.bo.filetype == "elixir" and os.execute("mix credo --version > /dev/null 2>&1") == 0)
+
         return {
-            sources = {
-                require('none-ls.diagnostics.eslint_d'),
-                require('none-ls.code_actions.eslint_d')
-            }
+            sources = sources
         }
     end,
 }
