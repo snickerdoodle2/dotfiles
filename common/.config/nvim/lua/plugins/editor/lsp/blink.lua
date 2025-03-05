542e41ca254d7535f657982fb7ca8a383cdcba5f
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-03-05 21:06:29 +0100

    Use blink-cmp coloring

--- common/.config/nvim/lua/plugins/editor/lsp/blink.lua
+++ common/.config/nvim/lua/plugins/editor/lsp/blink.lua
@@ -6,7 +6,6 @@ return {
     enabled = true,
     opts = {
         appearance = {
-            use_nvim_cmp_as_default = true,
             nerd_font_variant = 'mono',
             kind_icons = {
                 Color = "██",
