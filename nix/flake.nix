77cec43373403c9e50bffd940b07c1d707fa9fb9
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-05-19 15:46:21 +0200

    cursor not working lool

--- nix/flake.nix
+++ nix/flake.nix
@@ -29,7 +29,7 @@
           "obsidian"
           "microsoft-teams"
           "blender"
-          "cursor"
+          "visual-studio-code"
         ];
         masApps = {
           "Affinity Photo 2: Image Editor" = 1616822987;
