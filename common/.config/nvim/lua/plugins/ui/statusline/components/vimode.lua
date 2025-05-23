dec70e58de16d0f57d6ea8516af7f868c6a86e0b
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-05-23 21:54:14 +0200

    updated icon

--- common/.config/nvim/lua/plugins/ui/statusline/components/vimode.lua
+++ common/.config/nvim/lua/plugins/ui/statusline/components/vimode.lua
@@ -2,7 +2,7 @@ local utils = require('heirline.utils')
 
 local mode_display = {
     provider = function(self)
-        return " %2(" .. self.mode_names[self.mode] .. "%)"
+        return "󰮯 %2(" .. self.mode_names[self.mode] .. "%)"
     end,
     hl = function(self)
         local mode = self.mode:sub(1, 1)
