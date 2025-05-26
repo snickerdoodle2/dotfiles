6bfb7b5a8a4c41ed413f8deec1845de44a77d450
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-05-26 21:10:41 +0200

    Inlay hints

--- common/.config/nvim/lua/plugins/editor/lsp/lspconfig.lua
+++ common/.config/nvim/lua/plugins/editor/lsp/lspconfig.lua
@@ -28,6 +28,15 @@ return {
                 map('<C-s>', function() vim.lsp.buf.signature_help({ border = 'single' }) end, '[S]ignature Help')
                 map('<F2>', vim.lsp.buf.rename, 'Rename')
                 map('<F4>', vim.lsp.buf.code_action, 'Code Actions')
+
+                local client = vim.lsp.get_client_by_id(event.data.client_id)
+                if not client then
+                    return
+                end
+
+                if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+                    vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
+                end
             end
         })
 
