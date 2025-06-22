a8016ee538d5ee2e88c62d8c7029a4623b49dbf8
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-06-22 02:52:22 +0200

    feat: missing git options

--- home/programs/cli/git.nix
+++ home/programs/cli/git.nix
@@ -10,47 +10,54 @@
     userName = "Dominik Pilipczuk";
     userEmail = "63516830+snickerdoodle2@users.noreply.github.com";
 
-    extraConfig = {
-      core = {
-        compression = 9;
-        preloadindex = true;
-        whitespace = "error";
-      };
+    ignores = [
+      ".DS_Store"
+    ];
+  };
 
-      url."git@github.com:snickerdoodle2/".insteadOf = "me:";
-      url."git@github.com:".insteadOf = "gh:";
-
-      init.defaultBranch = "main";
-
-      status = {
-        branch = true;
-        showStash = true;
-        showUntrackedFiles = "all";
-        short = true;
-      };
-
-      push = {
-        autoSetupRemote = true;
-        default = "current";
-        followTags = true;
-      };
-
-      pull = {
-        default = "current";
-        rebase = true;
-      };
-
-      rebase = {
-        autoStash = true;
-        missingCommitsCheck = "warn";
-      };
-
-      merge = {
-        conflictstyle = "zdiff3";
-      };
-
-      log.abbrevCommit = true;
+  programs.git.extraConfig = {
+    core = {
+      compression = 9;
+      preloadindex = true;
+      whitespace = "error";
+      pager = "${pkgs.delta}/bin/delta";
     };
+
+    url."git@github.com:snickerdoodle2/".insteadOf = "me:";
+    url."git@github.com:".insteadOf = "gh:";
+
+    init.defaultBranch = "main";
+
+    interactive.diffFilter = "${pkgs.delta}/bin/delta --color-only";
+
+    status = {
+      branch = true;
+      showStash = true;
+      showUntrackedFiles = "all";
+      short = true;
+    };
+
+    push = {
+      autoSetupRemote = true;
+      default = "current";
+      followTags = true;
+    };
+
+    pull = {
+      default = "current";
+      rebase = true;
+    };
+
+    rebase = {
+      autoStash = true;
+      missingCommitsCheck = "warn";
+    };
+
+    merge = {
+      conflictstyle = "zdiff3";
+    };
+
+    log.abbrevCommit = true;
   };
 
   programs.git.delta = {
