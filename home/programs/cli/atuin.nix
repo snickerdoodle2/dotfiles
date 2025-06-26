6eaa87cee73071f9bfc97bbb868537e7a3ec1d53
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-06-26 21:59:30 +0200

    feat: atuin

---
+++ home/programs/cli/atuin.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  pkgs,
+  ...
+}: let
+  themes = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "atuin";
+    rev = "abfab12de743aa73cf20ac3fa61e450c4d96380c";
+    sha256 = "18544m6gz3n19gwz07r85mxpkflz6wybnffrhaid8wa237xfmwxp";
+  };
+in {
+  xdg.configFile."atuin/themes" = {
+    source = "${themes}/themes";
+  };
+
+  programs.atuin = {
+    enable = true;
+    package = pkgs.atuin;
+    settings = {
+      style = "auto";
+      theme.name = "catppuccin-mocha-mauve";
+    };
+  };
+}
