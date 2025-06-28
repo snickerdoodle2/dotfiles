7a0359e78bb8838b90231824480953157728fe81
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-06-28 12:54:01 +0200

    chore (nushell): refactor

--- home/programs/cli/shell/default.nix
+++ home/programs/cli/shell/default.nix
@@ -1,13 +1,23 @@
 {
   config,
   pkgs,
+  inputs,
   ...
-}: {
+}: let
+  pkgs-unstable = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system};
+in {
   imports = [
     ./atuin.nix
     ./direnv.nix
-    ./nushell.nix
   ];
+
+  programs.nushell = {
+    enable = true;
+    package = pkgs-unstable.nushell;
+    plugins = [
+      pkgs-unstable.nushellPlugins.gstat
+    ];
+  };
 
   programs.zoxide = {
     enable = true;
