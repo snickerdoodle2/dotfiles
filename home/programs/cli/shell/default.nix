d7eb666a7133fb1fd7dff477a8956bac7b88bb6f
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-06-29 14:49:16 +0200

    feat (nushell): theme inside $colors envvar

--- home/programs/cli/shell/default.nix
+++ home/programs/cli/shell/default.nix
@@ -11,6 +11,8 @@
     rev = "05987d258cb765a881ee1f2f2b65276c8b379658";
     sha256 = "13a2am30w1v8lz7drc04z3762jrywdqflfbn446iab6slfpw23dm";
   };
+
+  catppuccin-mocha = builtins.toFile "catppuccin_mocha.nu" (builtins.readFile "${themes}/themes/catppuccin_mocha.nu" + "\n$env.colors = $theme");
 in {
   imports = [
     ./atuin.nix
@@ -28,7 +30,7 @@ in {
       $env.LS_COLORS = (${pkgs.vivid}/bin/vivid generate catppuccin-mocha)
     '';
     extraConfig = ''
-      source ${themes}/themes/catppuccin_mocha.nu
+      source ${catppuccin-mocha}
     '';
   };
 
