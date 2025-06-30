51e59dbf20368a6b9c91be01bd4f9fe014dc8335
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-06-30 07:30:02 +0200

    feat: load cursor from github repo

--- home/hyprland/cursor/default.nix
+++ home/hyprland/cursor/default.nix
@@ -1,20 +1,19 @@
 {
   config,
-  lib,
+  pkgs,
   ...
 }: let
-  iconDirs = builtins.attrNames (lib.filterAttrs (_: v: v == "directory") (builtins.readDir ./.));
-
-  icons = builtins.listToAttrs (map (name: {
-      name = "${config.home.homeDirectory}/.local/share/icons/${name}";
-      value = {
-        source = ./. + "/${name}";
-        recursive = true;
-      };
-    })
-    iconDirs);
+  macosCursor = pkgs.fetchFromGitHub {
+    owner = "driedpampas";
+    repo = "macOS-hyprcursor";
+    rev = "94b831a2ac5acb9162862efb84102c2b88dc1fa2";
+    sha256 = "1hxa0c27w3vg5wyrzjkxxjq3l7gsxqmc2r05mg8r0a01j36ac6ak";
+  };
 in {
-  home.file = icons;
+  home.file.".local/share/icons/macOS" = {
+    source = "${macosCursor}/src/macOS (SVG)";
+    recursive = true;
+  };
   wayland.windowManager.hyprland.settings.env = [
     "HYPRCURSOR_THEME,MacOS"
     "HYPRCURSOR_SIZE,24"
