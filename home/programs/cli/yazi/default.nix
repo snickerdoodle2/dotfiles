ee7a03e1e86ab98141dde223a4ce8206c4c8d845
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-07-01 08:50:02 +0200

    feat (yazi): more config

--- home/programs/cli/yazi/default.nix
+++ home/programs/cli/yazi/default.nix
@@ -1,4 +1,15 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  batPlugin = pkgs.fetchFromGitHub {
+    owner = "mgumz";
+    repo = "yazi-plugin-bat";
+    rev = "4dea0a584f30247b8ca4183dc2bd38c80da0d7ea";
+    sha256 = "13b6hzgg4z6bhrnm5r5lpf1m3k13mncansdg5x2s0rcby9lvrxiq";
+  };
+in {
+  home.packages = with pkgs; [
+    resvg
+  ];
+
   programs.yazi = {
     enable = true;
     package = pkgs.yazi;
@@ -8,13 +19,44 @@
     };
     settings = {
       mgr = {
+        ratio = [1 3 4];
+        sort_by = "natural";
+        sort_sensitive = false;
         sort_dir_first = true;
-        sort_by = "alphabetical";
+        sort_translit = true;
+        scrolloff = 8;
+      };
+      plugin = {
+        append_previewers = [
+          {
+            mime = "text/*";
+            run = "bat";
+          }
+          {
+            mime = "*/xml";
+            run = "bat";
+          }
+          {
+            mime = "*/cs";
+            run = "bat";
+          }
+          {
+            mime = "*/javascript";
+            run = "bat";
+          }
+          {
+            mime = "*/x-wine-extension-ini";
+            run = "bat";
+          }
+        ];
       };
     };
     flavors = {
       catppuccin-mocha = ./catppuccin-mocha;
       catppuccin-latte = ./catppuccin-latte;
+    };
+    plugins = {
+      bat = batPlugin;
     };
   };
 }
