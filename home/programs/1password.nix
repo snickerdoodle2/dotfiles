fbc50c88cdf5991b980da8fee756018694ec6468
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-07-04 17:06:10 +0200

    feat (1password): change paths for darwin

--- home/programs/1password.nix
+++ home/programs/1password.nix
@@ -3,9 +3,18 @@
   lib,
   ...
 }: let
-  onePassPath = "~/.1password/agent.sock";
   inherit (lib) mkIf;
   inherit (pkgs.stdenv.hostPlatform) isDarwin;
+
+  onePassPath =
+    if isDarwin
+    then "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\""
+    else "~/.1password/agent.sock";
+
+  op-ssh-sign =
+    if isDarwin
+    then "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
+    else lib.getExe' pkgs._1password-gui "op-ssh-sign";
 in {
   home.packages = mkIf (!isDarwin) [
     pkgs._1password-gui
@@ -20,7 +29,7 @@ in {
 
   programs.git.extraConfig = {
     gpg.format = "ssh";
-    "gpg \"ssh\"".program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+    "gpg \"ssh\"".program = op-ssh-sign;
     commit.gpgsign = true;
     user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+yQBBLAqIPgDYfcIrXhgwfdE+XW4nqQCM/+ussS+dJ";
   };
