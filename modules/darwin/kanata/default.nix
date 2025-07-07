3dd8389f8ad08574395405c32e2767b0a627953b
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-07-08 00:23:47 +0200

    feat (kanata): karabiner daemons

--- modules/darwin/kanata/default.nix
+++ modules/darwin/kanata/default.nix
@@ -17,5 +17,19 @@
       StandardOutPath = "/Library/Logs/Kanata/kanata.out.log";
       StandardErrorPath = "/Library/Logs/Kanata/kanata.err.log";
     };
+    karabiner-vhiddaemon.serviceConfig = {
+      ProgramArguments = [
+        "/Library/Application Support/org.pqrs/Karabiner-DriverKit-VirtualHIDDevice/Applications/Karabiner-VirtualHIDDevice-Daemon.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Daemon"
+      ];
+      RunAtLoad = true;
+      KeepAlive = true;
+    };
+    karabiner-vhidmanager.serviceConfig = {
+      ProgramArguments = [
+        "/Applications/.Karabiner-VirtualHIDDevice-Manager.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Manager"
+        "activate"
+      ];
+      RunAtLoad = true;
+    };
   };
 }
