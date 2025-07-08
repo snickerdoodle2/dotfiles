1f87c10ab9e19aa7852abedf712aef1c915f578f
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-07-08 22:24:48 +0200

    feat (aerospace): movement

--- home/programs/darwin/aerospace.nix
+++ home/programs/darwin/aerospace.nix
@@ -27,18 +27,41 @@ in {
   };
 
   # https://github.com/nikitabobko/AeroSpace/issues/1012
-  skhd-keybinds = builtins.concatMap (i: let
-    key = toString (lib.mod i 10);
-  in [
-    {
-      inherit key;
-      modifiers = ["lalt"];
-      action = "${bin} workspace ${toString i}";
-    }
-    {
-      inherit key;
-      modifiers = ["lalt" "lshift"];
-      action = "${bin} move-node-to-workspace ${toString i}";
-    }
-  ]) (lib.range 1 10);
+  skhd-keybinds =
+    builtins.concatLists (
+      builtins.attrValues (
+        builtins.mapAttrs (key: dir: [
+          {
+            modifiers = ["lalt"];
+            inherit key;
+            action = "${bin} focus ${dir}";
+          }
+          {
+            modifiers = ["lalt" "lshift"];
+            inherit key;
+            action = "${bin} move ${dir}";
+          }
+        ])
+        {
+          h = "left";
+          j = "down";
+          k = "up";
+          l = "right";
+        }
+      )
+    )
+    ++ builtins.concatMap (i: let
+      key = toString (lib.mod i 10);
+    in [
+      {
+        inherit key;
+        modifiers = ["lalt"];
+        action = "${bin} workspace ${toString i}";
+      }
+      {
+        inherit key;
+        modifiers = ["lalt" "lshift"];
+        action = "${bin} move-node-to-workspace ${toString i}";
+      }
+    ]) (lib.range 1 10);
 }
