9731e2eed109547f2cdb2f4c7402cd09f01a9d82
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-07-14 10:01:17 +0200

    feat (darwin): packages

--- home/darwin.nix
+++ home/darwin.nix
@@ -1,8 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   home.stateVersion = "25.05";
   programs.home-manager.enable = true;
 
   targets.darwin.defaults."com.apple.dock".size-immutable = true;
+
+  home.packages = with pkgs; [
+    betterdisplay
+    spotify
+  ];
 
   imports = [
     ./programs/1password.nix
