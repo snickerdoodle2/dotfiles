82a51cc9263b5edcfab7f489139b370c8c0efb4e
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-07-14 21:09:58 +0200

    feat (nvim): conform update

--- config/nvim/lua/plugins/editor/lsp/conform.lua
+++ config/nvim/lua/plugins/editor/lsp/conform.lua
@@ -1,23 +1,27 @@
 return {
-	"stevearc/conform.nvim",
-	event = "LazyFile",
-	opts = {
-		format_on_save = {
-			timeout_ms = 500,
-			lsp_fallback = true,
-		},
-		formatters_by_ft = {
-			nix = { "alejandra" },
-			lua = { "stylua" },
-		},
-	},
-	keys = {
-		{
-			"<F3>",
-			function()
-				require("conform").format({ lsp_fallback = true, timeout_ms = 500 })
-			end,
-			desc = "Format buffer",
-		},
-	},
+    "stevearc/conform.nvim",
+    event = "LazyFile",
+    opts = {
+        format_on_save = {
+            timeout_ms = 500,
+            lsp_fallback = true,
+        },
+        formatters_by_ft = {
+            nix = { "alejandra" },
+            lua = { "stylua" },
+            javascript = { "prettier" },
+            typescript = { "prettier" },
+            svelte = { "prettier" },
+            css = { "prettier" },
+        },
+    },
+    keys = {
+        {
+            "<F3>",
+            function()
+                require("conform").format({ lsp_fallback = true, timeout_ms = 500 })
+            end,
+            desc = "Format buffer",
+        },
+    },
 }
