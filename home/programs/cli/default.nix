15cb387f81940f9b42aea43be0461d45edbffdc8
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-07-27 13:04:31 +0200

    feat (helix): formatting nix files

--- home/programs/cli/default.nix
+++ home/programs/cli/default.nix
@@ -14,7 +14,6 @@
   ];
 
   home.packages = with pkgs; [
-    alejandra
     fd
     fzf
     jq
--- home/programs/cli/helix.nix
+++ home/programs/cli/helix.nix
@@ -1,4 +1,9 @@
-{pkgs, inputs, config, ...}: let
+{
+  pkgs,
+  inputs,
+  config,
+  ...
+}: let
   pkgs-unstable = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system};
 in {
   programs.helix = {
@@ -16,17 +21,32 @@ in {
         };
       };
       keys.normal = {
-        esc = [ "collapse_selection" "keep_primary_selection" ];
+        esc = ["collapse_selection" "keep_primary_selection"];
         space = {
           g = [":new" ":insert-output ${config.programs.lazygit.package}/bin/lazygit" ":buffer-close!" ":redraw"];
           e = [":sh rm -f /tmp/yazi-path" ":insert-output ${config.programs.yazi.package}/bin/yazi %{buffer_name} --chooser-file=/tmp/yazi-path" ":open %sh{${pkgs.coreutils}/bin/cat /tmp/yazi-path}" ":redraw"];
         };
       };
     };
+    languages = {
+      language = [
+        {
+          name = "nix";
+          formatter = {
+            command = "${pkgs.alejandra}/bin/alejandra";
+            args = ["-"];
+          };
+          auto-format = true;
+        }
+      ];
+    };
     themes = {
       "catppuccin_black" = {
         "inherits" = "catppuccin_mocha";
-        "ui.background" = { fg = "text"; bg = "#000000";};
+        "ui.background" = {
+          fg = "text";
+          bg = "#000000";
+        };
       };
     };
   };
