3f5b9311e589c61c55dbad73d943edeaecd3baba
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-07-27 12:31:17 +0200

    feat (helix): change theme

--- home/programs/cli/helix.nix
+++ home/programs/cli/helix.nix
@@ -5,6 +5,7 @@ in {
     enable = true;
     package = pkgs-unstable.helix;
     settings = {
+      theme = "catppuccin_black";
       editor = {
         line-number = "relative";
         mouse = false;
@@ -19,6 +20,12 @@ in {
           g = [":new" ":insert-output ${config.programs.lazygit.package}/bin/lazygit" ":buffer-close!" ":redraw"];
           e = [":sh rm -f /tmp/yazi-path" ":insert-output ${config.programs.yazi.package}/bin/yazi %{buffer_name} --chooser-file=/tmp/yazi-path" ":open %sh{${pkgs.coreutils}/bin/cat /tmp/yazi-path}" ":redraw"];
         };
+      };
+    };
+    themes = {
+      "catppuccin_black" = {
+        "inherits" = "catppuccin_mocha";
+        "ui.background" = { fg = "text"; bg = "#000000";};
       };
     };
   };
