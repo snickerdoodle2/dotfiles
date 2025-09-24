4ee7847f38b5a526becfc61da8f14c05490b55fb
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-09-25 00:51:32 +0200

    feat (ghostty): config

--- home/programs/desktop/ghostty.nix
+++ home/programs/desktop/ghostty.nix
@@ -1,14 +1,35 @@
-{pkgs, ...}: let
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
   inherit (pkgs.stdenv.hostPlatform) isDarwin;
+  inherit (lib) mkOption types;
 in {
-  programs.ghostty = {
-    enable = true;
-    package =
-      if isDarwin
-      then null
-      else pkgs.ghostty;
-    settings = {
-      theme = "catppuccin-mocha";
+  options = {
+    ghosttyFontSize = mkOption {
+      type = types.int;
+      default = 15;
+    };
+  };
+  config = {
+    programs.ghostty = {
+      enable = true;
+      package =
+        if isDarwin
+        then null
+        else pkgs.ghostty;
+      settings = {
+        theme = "light:catppuccin-latte,dark:catppuccin-mocha";
+        font-family = "Berkeley Mono";
+        font-style = "Medium";
+        font-style-bold = "ExtraBold";
+        font-size = config.ghosttyFontSize;
+        window-padding-x = 8;
+        window-padding-y = 4;
+        command = "${pkgs.bash}/bin/bash -i -c ${config.programs.nushell.package}/bin/nu";
+      };
     };
   };
 }
