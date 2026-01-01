5e33145c57d1a31140ef153d957a39216ef28123
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2026-01-01 18:30:57 +0100

    feat: tmux

---
+++ home/programs/tmux.nix
@@ -0,0 +1,33 @@
+{pkgs, ...}: let
+  accent = "\"#a6e3a1\"";
+in {
+  programs.tmux = {
+    enable = true;
+    baseIndex = 1;
+    prefix = "C-Space";
+    newSession = true;
+    secureSocket = true;
+    customPaneNavigationAndResize = true;
+    keyMode = "vi";
+    terminal = "tmux-256color";
+  };
+  programs.tmux.plugins = [
+    {
+      plugin = pkgs.tmuxPlugins.minimal-tmux-status;
+      extraConfig = ''
+        set -g @minimal-tmux-bg ${accent}
+        set -g @minimal-tmux-fg "#11111b"
+        set -g @minimal-tmux-indicator-str " "
+        set -g pane-active-border-style fg=${accent}
+        set -sg escape-time 0
+      '';
+    }
+  ];
+  programs.tmux.extraConfig = ''
+    set -g exit-empty off
+    unbind '"'
+    unbind %
+    bind \\ split-window -h -c "#{pane_current_path}"
+    bind - split-window -v -c "#{pane_current_path}"
+  '';
+}
