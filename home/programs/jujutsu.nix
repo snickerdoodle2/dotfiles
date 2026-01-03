9f45b494255d5fa746f7e8402771518599d3d5df
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2026-01-03 12:55:27 +0100

    fix: expose mergiraf

--- home/programs/jujutsu.nix
+++ home/programs/jujutsu.nix
@@ -2,28 +2,16 @@
   pkgs,
   pkgs-unstable,
   ...
-}: let
-  jj = pkgs-unstable.jujutsu;
-  jjWrapped =
-    pkgs.runCommand "jj-wrapped" {
-      buildInputs = [pkgs.makeWrapper];
-    } ''
-      mkdir -p $out/bin
-      makeWrapper ${jj}/bin/jj $out/bin/jj \
-        --prefix PATH : ${pkgs.mergiraf}/bin
+}: {
+  home.packages = [
+    pkgs.mergiraf
+  ];
 
-        for bin in ${jj}/bin/*; do
-          if [ "$(basename $bin)" != "jj" ]; then
-            ln -s $bin $out/bin/$(basename $bin)
-          fi
-        done
-    '';
-in {
   # TODO: more configuration, commit signing
   # example: https://github.com/jj-vcs/jj/discussions/5812
   programs.jujutsu = {
     enable = true;
-    package = jjWrapped;
+    package = pkgs-unstable.jujutsu;
     settings = {
       user = {
         name = "Dominik Pilipczuk";
