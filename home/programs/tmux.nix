7d6b8ad6433618371e707de0fcf5f5d0ec8102e0
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2026-01-05 13:25:02 +0100

    feat (tmux): plugins & config changes

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -13,6 +13,8 @@ in {
     mouse = true;
   };
   programs.tmux.plugins = [
+    pkgs.tmuxPlugins.sensible
+    pkgs.tmuxPlugins.yank
     {
       plugin = pkgs.tmuxPlugins.minimal-tmux-status;
       extraConfig = ''
@@ -20,8 +22,6 @@ in {
         set -g @minimal-tmux-fg "#11111b"
         set -g @minimal-tmux-indicator-str "#S"
         set -g @minimal-tmux-right false
-        set -g pane-active-border-style fg=${accent}
-        set -sg escape-time 0
       '';
     }
   ];
@@ -31,5 +31,21 @@ in {
     unbind %
     bind \\ split-window -h -c "#{pane_current_path}"
     bind - split-window -v -c "#{pane_current_path}"
+
+    set-window-option -g mode-keys vi
+    bind-key -T copy-mode-vi v send-keys -X begin-selection
+    bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+    bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+    set -g pane-active-border-style fg=${accent}
+
+    bind -N "Select pane to the left of the active pane" h select-pane -L
+    bind -N "Select pane to the left of the active pane" C-h select-pane -L
+    bind -N "Select pane below the active pane" j select-pane -D
+    bind -N "Select pane below the active pane" C-j select-pane -D
+    bind -N "Select pane above the active pane" k select-pane -U
+    bind -N "Select pane above the active pane" C-k select-pane -U
+    bind -N "Select pane to the right of the active pane" l select-pane -R
+    bind -N "Select pane to the right of the active pane" C-l select-pane -R
   '';
 }
