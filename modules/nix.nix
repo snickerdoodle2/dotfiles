08c6db2060a9f590226505dcd15589b4f0fc7940
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2026-01-09 21:27:07 +0100

    feat: enable pipe operator

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,13 +1,8 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+{lib, ...}: {
   nixpkgs.config.allowUnfree = lib.mkDefault false;
 
   nix.settings = {
-    experimental-features = ["nix-command" "flakes"];
+    experimental-features = ["nix-command" "flakes" "pipe-operators"];
     auto-optimise-store = true;
 
     trusted-users = ["domi"];
