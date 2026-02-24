81bdf85aadc58a3177e829de94bde4fe50032fa5
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2026-02-24 19:47:02 +0100

    feat: enable pipe-operators

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -1,5 +1,4 @@
 {lib, ...}: {
   nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings.experimental-features = ["nix-command" "flakes" "pipe-operators"];
 }
-
