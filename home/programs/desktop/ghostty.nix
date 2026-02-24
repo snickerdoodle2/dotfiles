21f50396fc9003acb1111a1ca8529e8ab9be5a74
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2026-02-24 19:57:19 +0100

    feat (ghostty): move config from `rewrite` branch

--- home/programs/desktop/ghostty.nix
+++ home/programs/desktop/ghostty.nix
@@ -20,36 +20,10 @@ in {
         if isDarwin
         then null
         else pkgs.ghostty;
-      themes = {
-        catppuccin-mocha = {
-          palette = [
-            "0=#45475a"
-            "1=#f38ba8"
-            "2=#a6e3a1"
-            "3=#f9e2af"
-            "4=#89b4fa"
-            "5=#f5c2e7"
-            "6=#94e2d5"
-            "7=#a6adc8"
-            "8=#585b70"
-            "9=#f38ba8"
-            "10=#a6e3a1"
-            "11=#f9e2af"
-            "12=#89b4fa"
-            "13=#f5c2e7"
-            "14=#94e2d5"
-            "15=#bac2de"
-          ];
-          background = "000000";
-          foreground = "cdd6f4";
-          cursor-color = "f5e0dc";
-          cursor-text = "11111b";
-          selection-background = "1e1e2e";
-          selection-foreground = "cdd6f4";
-        };
-      };
+      clearDefaultKeybinds = true;
       settings = {
-        theme = "light:catppuccin-latte,dark:catppuccin-mocha";
+        theme = "Catppuccin Mocha";
+        background = "000000";
         font-family = "Berkeley Mono";
         font-style = "Medium";
         font-style-bold = "ExtraBold";
@@ -57,11 +31,14 @@ in {
         window-padding-x = 8;
         window-padding-y = 4;
         command = "${pkgs.bash}/bin/bash -i -c \"${config.programs.zellij.package}/bin/zellij a -c main\"";
-
+        confirm-close-surface = false;
         macos-option-as-alt = "left";
         keybind = [
-          "alt+left=unbind"
-          "alt+right=unbind"
+          "ctrl+shift+c=copy_to_clipboard"
+          "ctrl+shift+v=paste_from_clipboard"
+          "ctrl+==increase_font_size:1"
+          "ctrl+-=decrease_font_size:1"
+          "ctrl+0=reset_font_size"
         ];
       };
     };
