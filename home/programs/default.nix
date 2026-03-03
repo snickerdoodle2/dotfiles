ec8840295617f353fa059003db1c5e61d364f64e
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2026-03-03 22:50:48 +0100

    feat: cli programs

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -1,4 +1,13 @@
-{...}: {
+{pkgs, ...}: {
+  home.packages = [
+    pkgs.dust
+    pkgs.eva
+    pkgs.fd
+    pkgs.fzf
+    pkgs.ripgrep
+    pkgs.wget
+  ];
+
   imports = [
     ./bat.nix
     ./dms.nix
